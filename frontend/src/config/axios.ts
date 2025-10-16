a1b32b872e0dd614c42f6abb0320097739899442
captain-adi <maniramp554@gmail.com>
2025-10-16 19:57:18 +0530

    feat: basic setup for frontend

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,7 +1,7 @@
 import axiosR from "axios";
 
 const axios = axiosR.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api/v1",
   withCredentials: true,
 });
 
